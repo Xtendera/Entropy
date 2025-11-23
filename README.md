5d40266576209aa4a0255a7d51ddc4b515fb8179
xtendera <geektraindev@gmail.com>
2025-11-22 18:59:11 -0600

    Replaced Mason buildsystem with CMake For compatability reasons

---
+++ README.md
@@ -0,0 +1,6 @@
+# Entropy
+A WIP game framework.
+
+## Credits
+
+This project uses the SDL3 library, and CMake.
--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,5 @@
 #include <iostream>
 
 int main() {
-  std::cout << "App is running.\n";
-  return 0;
+  std::cout << "Test 123";
 }
